11e61b55955772e4b879737332838d7a71a84192
Ahmed Muawia <amuawia666@gmail.com>
2024-10-24 13:27:01 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -19,16 +19,15 @@ def cache(func: Callable) -> Callable:
         """
          wrapper function for caching the output.
         """
-        key = "count:{}".format(url)
-        r.incr(key)
+        r.incr(f'count:{url}')
 
-        cached_page = r.get("cache:{}".format(url))
+        cached_page = r.get(f'cache:{url}')
         if cached_page:
             return cached_page.decode('utf-8')
 
         response = func(url)
-        if response:
-            r.setex("cache:{}".format(url), 10, response)
+        r.set(f'count:{url}', 0)
+        r.setex("cache:{}".format(url), 10, response)
 
         return response
 
@@ -44,8 +43,4 @@ def get_page(url: str) -> str:
     the key "count:{url}" and cache the result with an
     expiration time of 10 seconds.
     """
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return None
+    return requests.get(url).text
